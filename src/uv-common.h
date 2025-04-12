reitowo/libuv

src: win pipe add unix domain socket support

src/uv-common.h
@@ -122,6 +122,7 @@ enum {
   /* Only used by uv_pipe_t handles. */
   UV_HANDLE_NON_OVERLAPPED_PIPE         = 0x01000000,
   UV_HANDLE_PIPESERVER                  = 0x02000000,
+  UV_HANDLE_WIN_UNIX_DOMAIN_SOCKET                = 0x04000000,
 
   /* Only used by uv_tty_t handles. */
   UV_HANDLE_TTY_READABLE                = 0x01000000,

chensongpoixs/clibuv

src/uv-common.h
@@ -123,6 +123,7 @@ enum {
   /* Only used by uv_pipe_t handles. */
   UV_HANDLE_NON_OVERLAPPED_PIPE         = 0x01000000,
   UV_HANDLE_PIPESERVER                  = 0x02000000,
+  UV_HANDLE_WIN_UDS_PIPE                = 0x04000000,
 
   /* Only used by uv_tty_t handles. */
   UV_HANDLE_TTY_READABLE                = 0x01000000,